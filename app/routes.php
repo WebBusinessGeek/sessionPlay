fc28e186e2d393d26f42175b1cfa6805c2b51843
Kevin Washington <kevw12188@gmail.com>
2014-09-29 20:39:11 -0400

    example setup

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,44 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+/**
+* Show api to store, check, retrieve, and remove a session instance
+*
+**/
+
+Route::get('/store', function(){
+
+	Session::put('keyName', 'valueName');
+	return 'Data is stored';
+	
+	
+});
+
+Route::get('/check', function(){
+	
+	if(Session::has('keyName')){
+		return 'Session data is stored';
+	}
+	else{
+		return 'Session data is NOT stored';	
+	}
+
+});
+
+Route::get('/retrieve', function(){
+	
+	$sessionQuery = Session::get('keyName');
+
+	dd($sessionQuery);
+
+});
+
+Route::get('/remove', function(){
+
+	$sessionRemoveQuery = Session::pull('keyName');
+
+	return 'This data has been removed: ' . $sessionRemoveQuery;
+
+});
